5e8c9071f1bb2c0a9b8d1670854b4f698637160a
Levi Blackstone <levi@pulumi.com>
2020-03-02 16:25:37 -0700

    WIP

--- pkg/gen/typegen.go
+++ pkg/gen/typegen.go
@@ -832,17 +832,17 @@ func makeSchemaTypeSpec(resourceType, propName string, prop map[string]interface
 		return pschema.TypeSpec{Type: "string"}
 	case v1beta1JSONSchemaPropsOrBool:
 		return pschema.TypeSpec{OneOf: []pschema.TypeSpec{
-			{Ref: "#/types/kubernetes:apiextensions/v1beta1/JSONSchemaProps:JSONSchemaProps"},
+			{Ref: "#/types/kubernetes:apiextensions/v1beta1:JSONSchemaProps"},
 			{Type: "boolean"},
 		}}
 	case v1JSONSchemaPropsOrBool:
 		return pschema.TypeSpec{OneOf: []pschema.TypeSpec{
-			{Ref: "#/types/kubernetes:apiextensions/v1/JSONSchemaProps:JSONSchemaProps"},
+			{Ref: "#/types/kubernetes:apiextensions/v1:JSONSchemaProps"},
 			{Type: "boolean"},
 		}}
 	case v1beta1JSONSchemaPropsOrArray:
 		return pschema.TypeSpec{OneOf: []pschema.TypeSpec{
-			{Ref: "#/types/kubernetes:apiextensions/v1beta1/JSONSchemaProps:JSONSchemaProps"},
+			{Ref: "#/types/kubernetes:apiextensions/v1beta1:JSONSchemaProps"},
 			{
 				Type:  "array",
 				Items: &pschema.TypeSpec{Ref: "pulumi.json#/Any"},
@@ -850,7 +850,7 @@ func makeSchemaTypeSpec(resourceType, propName string, prop map[string]interface
 		}}
 	case v1JSONSchemaPropsOrArray:
 		return pschema.TypeSpec{OneOf: []pschema.TypeSpec{
-			{Ref: "#/types/kubernetes:apiextensions/v1/JSONSchemaProps:JSONSchemaProps"},
+			{Ref: "#/types/kubernetes:apiextensions/v1:JSONSchemaProps"},
 			{
 				Type:  "array",
 				Items: &pschema.TypeSpec{Ref: "pulumi.json#/Any"},
